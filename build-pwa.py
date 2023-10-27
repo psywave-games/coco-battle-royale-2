97f26ecc455a318e1655496f144c28c14a10bbfe
RodrigoDornelles <rodrigo@dornelles.me>
2023-10-26 21:16:34 -0300

    build: improve python build html

--- build-pwa.py
+++ build-pwa.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from base64 import b64encode as b64
+import requests
 import sys
 
 title = 'Coco Battle Royale II'
@@ -11,9 +12,16 @@ if '--jap' in sys.argv:
     input = './bin/kokobatoru.nes'
     output = './html/jap.html'
 
+if '--download' in sys.argv:
+    for file_name in ['nes.min.js']:
+        cdn = 'https://cdn.jsdelivr.net/gh/takahirox/nes-js@v0.0.1/build'
+        with requests.get(f'{cdn}/{file_name}', stream=True) as download:
+            file_name = file_name.replace(r'.min.js', '.js')
+            open(f'./html/{file_name}', "w").write(download.text)
+
 rom = b64(open(input, "rb").read()).decode()
 html = open("./res/index.html", "r").read()
 html = html.replace(r'{{game}}', title).replace(r'{{rom}}', rom)
-
 open(output, "w").write(html)
+
 print(title)
