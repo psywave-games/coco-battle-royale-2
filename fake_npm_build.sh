7cdafd325761146799074728454b5902f861bfa7
RodrigoDornelles <rodrigo@dornelles.me>
2023-11-02 16:23:37 -0300

    feat: add favicon to web game

--- fake_npm_build.sh
+++ fake_npm_build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 mkdir -p html
 cp ./bin/*.nes ./html
+cp ./res/*.ico ./html
 cp ./res/index.html ./html
 echo '<!DOCTYPE html><html><head><meta http-equiv="refresh" content="1;url=https://psywave-games.github.io/coco-battle-royale-2/"></head></html>' > ./html/404.html
