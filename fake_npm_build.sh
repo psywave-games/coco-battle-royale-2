777e427b4929009705340794b22f2f94b2629d2c
RodrigoDornelles <rodrigo@dornelles.me>
2023-11-02 19:37:02 -0300

    feat: transform in to the pwa

--- fake_npm_build.sh
+++ fake_npm_build.sh
@@ -2,5 +2,8 @@
 mkdir -p html
 cp ./bin/*.nes ./html
 cp ./res/*.ico ./html
+cp ./res/*.png ./html
+cp ./res/*.js ./html
 cp ./res/index.html ./html
+cp ./manifest.json ./html
 echo '<!DOCTYPE html><html><head><meta http-equiv="refresh" content="1;url=https://psywave-games.github.io/coco-battle-royale-2/"></head></html>' > ./html/404.html
---
+++ res/pwa.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'kokobatoru-2';
+
+const urlsToCache = [
+    'index.html',
+    'favicon.ico',
+    'fceux.js',
+    'fceux.wasm',
+    'cocobattleroyale.nes',
+    'galinharoyale.nes',
+    'kokobatoru.nes'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response || !event.request) {
+        return response;
+      }
+      return fetch(event.request).then((fetchResponse) => {
+        if (!fetchResponse || fetchResponse.status !== 200 || fetchResponse.type !== 'basic') {
+          return fetchResponse;
+        }
+
+        const responseToCache = fetchResponse.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return fetchResponse;
+      });
+    })
+  );
+});
