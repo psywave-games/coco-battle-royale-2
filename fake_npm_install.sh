46feeb49dd569dc1630eed67cab006aba01df1b0
rodrigodornelles <rodrigo@dornelles.me>
2023-11-08 12:31:44 -0300

    feat: improve mobile responsive

--- fake_npm_install.sh
+++ fake_npm_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 mkdir -p html
-URL_GAMEPAD=https://rodrigodornelles.github.io/npm-gamepadzilla/gamepadzilla.js
+URL_GAMEPAD=https://cdn.jsdelivr.net/npm/gamepadzilla@1/dist/gamepadzilla.js
 URL_FCEUX_JS=https://cdn.jsdelivr.net/npm/em-fceux@2.2.0/dist/fceux.min.js
 URL_FCEUX_WASM=https://cdn.jsdelivr.net/npm/em-fceux@2.2.0/dist/fceux.wasm
 (curl -fsSL $URL_GAMEPAD || wget -qO- $URL_GAMEPAD) > html/gamepadzilla.js
